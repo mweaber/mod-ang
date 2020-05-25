2db4c12553d249afd3c54e3d919d03fdf8dd8dab
Matthew Weaber <matthewweaber89@gmail.com>
2020-05-25 14:04:03 -0400

    generator working

--- password-gen/src/app/app.component.ts
+++ password-gen/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,62 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'password-gen';
+
+  password = '**********';
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  length = 0;
+
+  onButtonClick() {
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+    if (this.includeLetters){
+      validChars += letters;
+    }
+    if (this.includeNumbers){
+      validChars += numbers;
+    }
+    if (this.includeSymbols){
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+
+    for (let i = 0; i < this.length; i++){
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+
+    this.password = generatedPassword;
+
+  }
+
+  onChangeLength(value: string) {
+    const parseValue = parseInt(value);
+
+    if (!isNaN(parseValue)) {
+      this.length = parseValue;
+    }
+  }
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols
+  }
+
+  // getName() {
+  //   return 'Issa me! Mario'
+  // }
+
 }
