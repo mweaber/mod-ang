5b3bbcb958f3881347f44d8af416c15d24a3b1c9
Matthew Weaber <matthewweaber89@gmail.com>
2020-06-16 11:59:31 -0400

    finished up and deployed

--- post-cards/src/app/card/card.component.ts
+++ post-cards/src/app/card/card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
 
 @Component({
   selector: 'app-card',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
+
+  @Input() title = ""
+  @Input() imageUrl = ""
+  @Input() content = ""
+  @Input() username = ""
 
   constructor() { }
 
