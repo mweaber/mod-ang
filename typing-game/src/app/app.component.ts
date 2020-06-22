5672616448799fce6b2c8aa85964e8ef4f244b8b
Matthew Weaber <matthewweaber89@gmail.com>
2020-06-22 18:30:57 -0400

    wip

--- typing-game/src/app/app.component.ts
+++ typing-game/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'typing-game';
+
+  randomText = lorem.sentence();
+  typedText: string = '';
+
+  onChange(value: string) {
+    console.log(value)
+
+    this.typedText = value
+  }
+
+  compare(randomLetter:string, enteredLetter:string){
+    if (!enteredLetter) {
+      return 'pending'
+    } 
+
+    return randomLetter === enteredLetter ? 'correct' : 'incorrect'
+  }
+
+  getNew(){
+    this.randomText = lorem.sentence();
+  }
+
 }
